db797c5d60fee43cd505ef4ca3cf674eb2e4f999
Kasper de Bruin <kasperbruin8@gmail.com>
2020-08-01 17:07:55 +0200

    Database set up as an scriptableObject and basic reading To Json

---
+++ Assets/Scripts/DatabaseToSave.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Database", menuName = "JsonUtility/Database")]
+public class DatabaseToSave : ScriptableObject
+{
+	public DatabaseData databaseData;
+}
+
+[Serializable]
+public class DatabaseData
+{
+	public Data[] numbersToSave = new Data[5];
+}
+
+[Serializable]
+public class Data
+{
+	public int numbersToSave;
+
+}
---
+++ Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SaveLoad : MonoBehaviour
+{
+	public DatabaseToSave databaseToSave;
+
+	[ContextMenu("Save Database")]
+	private void Save()
+	{
+		if (databaseToSave != null)
+		{
+			string text = JsonUtility.ToJson(databaseToSave.databaseData);
+			Debug.Log(text + "Json String");
+		}
+		else
+		{
+			Debug.LogError("database not set!");
+		}
+	}
+}
+
+
