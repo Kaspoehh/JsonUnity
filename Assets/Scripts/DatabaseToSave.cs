5f4e068eaed085c2a600fe1977099b07562f8426
Kasper de Bruin <kasperbruin8@gmail.com>
2020-08-02 10:45:09 +0200

    Saving and loading to json added

--- Assets/Scripts/DatabaseToSave.cs
+++ Assets/Scripts/DatabaseToSave.cs
@@ -18,5 +18,4 @@ public class DatabaseData
 public class Data
 {
 	public int numbersToSave;
-
 }
--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -1,22 +1,36 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 public class SaveLoad : MonoBehaviour
 {
-	public DatabaseToSave databaseToSave;
+    public DatabaseToSave database;
+    public string savePath;
 
-	[ContextMenu("Save Database")]
-	private void Save()
-	{
-		if (databaseToSave != null)
-		{
-			string text = JsonUtility.ToJson(databaseToSave.databaseData);
-			Debug.Log(text + "Json String");
+    [ContextMenu("Save")]
+    public void Save()
+    {
+        Debug.Log(savePath + "The Save Path");
+		string saveData = JsonUtility.ToJson(database, true);
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(string.Concat(Application.dataPath, savePath));
+		bf.Serialize(file, saveData);
+		file.Close();
+    }
+
+    [ContextMenu("Load")]
+    public void Load()
+    {
+        if (File.Exists(string.Concat(Application.persistentDataPath, savePath)))
+        {
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(string.Concat(Application.dataPath, savePath), FileMode.Open);
+			JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), database);
+			file.Close();
 		}
-		else
-		{
-			Debug.LogError("database not set!");
-		}
-	}
+    }
+
 }
 
 
